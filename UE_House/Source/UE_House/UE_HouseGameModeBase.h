85b49f58cf84c33fe038cffaeeeb9f933e9e87e2
AprilDeFeu <aprildefeu@protonmail.com>
2022-10-12 16:58:07 -0400

    Made basement

---
+++ UE_House/Source/UE_House/UE_HouseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE_HouseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_HOUSE_API AUE_HouseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
